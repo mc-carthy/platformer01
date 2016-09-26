9f33ec0a8e20c538caa4322e2a0620904a6732b2
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-26 21:33:12 +0100

    Add Rotator

--- Assets/scripts/Rotator.cs
+++ Assets/scripts/Rotator.cs
@@ -3,5 +3,13 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour {
 
+	private float speed = 200;
 
+	private void Update () {
+		transform.localEulerAngles = new Vector3 (
+			transform.localEulerAngles.x,
+			transform.localEulerAngles.y,
+			transform.localEulerAngles.z + speed * Time.deltaTime
+		);
+	}
 }
